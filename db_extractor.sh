e03be920cd67ad3a33cea98c23036e07d2785583
Frederic Suter <frederic.suter@cc.in2p3.fr>
2016-09-19 14:50:17 +0200

    cosmetics

--- db_extractor.sh
+++ db_extractor.sh
@@ -1,7 +1,7 @@
 #! /bin/bash -u
 ##############################################################################
 # Copyright (c) Centre de Calcul de l'IN2P3 du CNRS, CREATIS                 #
-# Contributor(s) : Frédéric SUTER, Mohammad Mahdi BAZM (2015)                #
+# Contributor(s) : Frédéric SUTER (2015-2016), Mohammad Mahdi BAZM (2015)    #
 #                                                                            #
 # This program is free software; you can redistribute it and/or modify it    #
 # under the terms of the license (GNU LGPL) which comes with this code.      #
@@ -21,9 +21,8 @@ output="db_dump.csv"
 if [ ! -f "$output" ]
 then
     info "File $output does not exist. Create it."
-    echo "JobId Command Name Site CreationTime QueuingDuration "\
-"DownloadStartTime DownloadDuration ComputeStartTime ComputeDuration "\
-"UploadStartTime UploadDuration TotalDuration logFile" > $output
+    echo "JobId Command Name Site CreationTime QueuingDuration DownloadStartTime DownloadDuration ComputeStartTime" \
+    "ComputeDuration UploadStartTime UploadDuration TotalDuration logFile" > $output
 fi
 
 # Prepare the SQL query
@@ -39,11 +38,8 @@ DATEDIFF('SECOND',UPLOAD, END_E) as UPLOAD_TIME,
 DATEDIFF('SECOND',DOWNLOAD,END_E) as TOTAL_TIME, FILE_NAME
 from JOBS WHERE STATUS='COMPLETED' ORDER BY ID"
 
-
-java -cp ${H2DRIVER} org.h2.tools.Shell \
-     -url "jdbc:h2:${LOG2SIM_LOGS}/${workflow_dir}/db/db/jobs" \
-     -user gasw -password gasw -sql "$sql_query" | \
-     sed -e '1d' -e '$d' -e 's/ *| */ /g' >> $output \
+java -cp ${H2DRIVER} org.h2.tools.Shell -url "jdbc:h2:${LOG2SIM_LOGS}/${workflow_dir}/db/db/jobs" \
+     -user gasw -password gasw -sql "$sql_query" | sed -e '1d' -e '$d' -e 's/ *| */ /g' >> $output \
 || info "SQL query failed."
 
 sed '1d' $output | while read line
@@ -65,33 +61,26 @@ do
 
     if [[ $3 == null ]] 
     then	
-	input_log=$LOG2SIM_LOGS/$workflow_dir/out/$8.sh.out
-	info "\tBad entry for job $1. Look to " \
-	    "${input_log} to correct it".
-	machine_name=$(awk -F'=' '/^HOSTNAME/ {print $NF}' $input_log)
-	site=$(awk -F'=' '/^SITE_NAME/ {print $NF}' $input_log |\
-               tr '[:lower:]' '[:upper:]')
+      input_log=$LOG2SIM_LOGS/$workflow_dir/out/$8.sh.out
+      info "\tBad entry for job $1. Look to ${input_log} to correct it".
+	    machine_name=$(awk -F'=' '/^HOSTNAME/ {print $NF}' $input_log)
+	    site=$(awk -F'=' '/^SITE_NAME/ {print $NF}' $input_log | tr '[:lower:]' '[:upper:]')
 	
-	if [[ $machine_name == "" ]]
-	then
-	    # means the log file is absent
-	    info "\tMissing log file, discard this job"
-	    sed "/$line/d" -i $output
-	else
-	    download_duration=$(awk '/] Input download/''{print}' $input_log | \
-		awk '{print $(NF-1)}')
-	    compute_time=$(awk '/] Execution time:/''{print}' $input_log | \
-		awk '{print $(NF-1)}')
-	    upload_duration=$(awk '/] Results upload/''{print}' $input_log | \
-		awk '{print $(NF-1)}')    
-	    total_time=$(awk '/] Total running/''{print}' $input_log | \
-		awk '{print $(NF-1)}')
-	    compute_start=$(($6 + $download_duration))
-	    upload_start=$(($compute_start + $compute_time))
-	    new_line=$(echo -e $1 $2 $machine_name $site $5 $6 $7 \
-		$download_duration $compute_start $compute_time $upload_start \
-		$upload_duration $total_time $8)
-	    sed "s/$line/$new_line/g" -i $output
-	fi
-    fi
+    	if [[ $machine_name == "" ]]
+	    then
+	      # means the log file is absent
+	      info "\tMissing log file, discard this job"
+	      sed "/$line/d" -i $output
+	    else
+	      download_duration=$(awk '/] Input download/''{print}' $input_log | awk '{print $(NF-1)}')
+	      compute_time=$(awk '/] Execution time:/''{print}' $input_log | awk '{print $(NF-1)}')
+	      upload_duration=$(awk '/] Results upload/''{print}' $input_log | awk '{print $(NF-1)}')
+  	    total_time=$(awk '/] Total running/''{print}' $input_log | awk '{print $(NF-1)}')
+	      compute_start=$(($6 + $download_duration))
+	      upload_start=$(($compute_start + $compute_time))
+	      new_line=$(echo -e $1 $2 $machine_name $site $5 $6 $7 $download_duration $compute_start $compute_time \
+        $upload_start $upload_duration $total_time $8)
+  	    sed "s/$line/$new_line/g" -i $output
+  	fi
+  fi
 done
